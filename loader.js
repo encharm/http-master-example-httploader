c3fb267ab8ec0db8bb6059fa8fd8544e72bd45af
Damian Kaczmarek <rush@rushbase.net>
2014-01-27 00:59:01 +0100

    Fix argument

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
 var request = require('request');
 
-module.exports = function(err, data, finish) {
+module.exports = function(argv, data, finish) {
 	var parsed = JSON.parse(data);
 	request(parsed.url, function(err, data) {
 		finish(err, JSON.parse(data.body));
